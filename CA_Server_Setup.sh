593b417859854f52827e13440a0e63d45a3dea5f
Spencer Foster <108783363+spurfff@users.noreply.github.com>
2023-12-16 20:39:12 -0800

    Create CA_Server_Setup.sh
    - Initial commit.
    - The comments at the top are variable names from the ~/easy-rsa/pki/openssl-easyrsa.cnf. Just for my own memory
    - This attempts to first inform the user what will currently be included in the CA Certificate Subject Line
    - Intending on building up more functions and features
    - Arbitrarily began with a function that will allow a user to choose what information they want to add into the Subject Line of the certificate

---
+++ CA_Server_Setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#[ policy_anything ]
+#countryName             = optional
+#stateOrProvinceName     = optional
+#localityName            = optional
+#organizationName        = optional
+#organizationalUnitName  = optional
+#commonName              = supplied
+#name                    = optional
+#emailAddress            = optional
+
+function vars_setup() {
+        # Some Global Variables
+        policies=(
+                "countryName"
+                "stateOrProvinceName"
+                "localityName" "organizationName"
+                "organizationalUnitName"
+                "commonName"
+                "name"
+                "emailAddress"
+        )
+        keyword="optional"
+        bold='\e[1;37m'
+        underline='\e[4;37m'
+        reset='\e[0m'
+        flourish="**********"
+
+        # Print the title of the currently running fuction
+        title="\n${flourish}${bold}${underline}Vars Setup${reset}${flourish}\n"
+
+
+        echo -e "$title"
+        sleep 2
+
+        echo -e "\n - The purpose of this portion of the script is to add information to the Subject line of the CA Certificate file - \n"
+        sleep 4
+
+        echo -e "\n - We'll do this by switching on some parameters that you added earlier - \n"
+        sleep 4
+}
+vars_setup
